b3796544fce99758b6c4ac7f26c334bc00df9782
diego <50971801+superdiego28@users.noreply.github.com>
2019-07-10 21:57:56 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,52 +1,53 @@
 package com.company;
 
 
+
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-
+/**
+ *
+ * @author Claudia Barrios
+ */
 public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String salida="";
-        for(int i=0;i<16;i++){
-            try {
-                URL url = new URL("https://swapi.co/api/people/"+(i+1));//your url i.e fetch data from .
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setRequestProperty("Accept", "application/json");
-                conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-                if (conn.getResponseCode() != 200) {
-                    throw new RuntimeException("Failed : HTTP Error code : "
-                            + conn.getResponseCode());
-                }
-                InputStreamReader in = new InputStreamReader(conn.getInputStream());
-                BufferedReader br = new BufferedReader(in);
-                String output;
-                while ((output = br.readLine()) != null) {
-                    //System.out.println(output);
-                    salida+=output;
-                }
-                conn.disconnect();
-            } catch (Exception e) {
-                System.out.println("Exception in NetClientGet:- " + e);
+        String salida ="";
+        try {
+
+            URL url = new URL("https://simplifiedcoding.net/demos/marvel/");//your url i.e fetch data from .
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP Error code : "
+                        + conn.getResponseCode());
             }
-            JsonParser jsonParser = new JsonParser();
-            JsonObject obj = jsonParser.parse(salida).getAsJsonObject();
-            //System.out.println(obj);
-            System.out.println("Nombre: "+obj.get("name")+", Altura: "+obj.get("height")+", Peso: "+obj.get("mass")+", Color Cabello: "+obj.get("hair_color")+", Color Piel: "+obj.get("skin_color"));
-            System.out.println("Color de Ojos: "+obj.get("eye_color")+", Cumpleaños: "+obj.get("birth_year")+", Genero: "+obj.get("gender")+", Tarea: "+obj.get("homeworld"));
-            System.out.println();
-            salida="";
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+            BufferedReader br = new BufferedReader(in);
+            String output;
+            while ((output = br.readLine()) != null) {
+                salida+=output;
+            }
+            conn.disconnect();
+
+        } catch (Exception e) {
+            System.out.println("Exception in NetClientGet:- " + e);
+        }
+        JsonParser jsonParser = new JsonParser();
+        JsonArray array = jsonParser.parse(salida).getAsJsonArray();
+        //System.out.println(array.toString());
+        for(int i=0;i<array.size();i++){
+            JsonObject item = array.get(i).getAsJsonObject();
+            System.out.println("Alias: "+item.get("name")+", Nombre: "+item.get("realname")+", Equipo: "+item.get("team")+", Año: "+item.get("firstappearance")+", Creada Por: "+item.get("createdby")+", Publicada Por: "+item.get("publisher")+", Url Imagen: "+item.get("imageurl")+", Biografia: "+item.get("bio"));
         }
     }
 
 }
-
